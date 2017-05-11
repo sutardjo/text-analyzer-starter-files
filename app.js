a1180fc2cd6d93f28723f1317b3f5970e5b92216
sutardjo <dodiet@gmail.com>
2017-05-10 21:00:44 -0500

    Math for evaluating text

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// your code here!
+var wordArray = [];
+var textToEvaluate;
+var wordCountAll;
+var wordCountUnique;
+var wordArrayUnique = [];
+var averageLengthOfWords;
+
+function doEvaluation() {
+	$('#submitButton').click(function() {
+		textToEvaluate = $('#textArea').val();
+		wordArray = textToEvaluate.split(" ");
+		wordCountAll = wordArray.length;
+		wordCountUnique = uniqueWords();
+		averageLengthOfWords = averageLength();
+	})
+}
+
+// Creates unique word array and counts words
+function uniqueWords() {
+	$.each(wordArray, function(i, el) {
+		if ($.inArray(el, wordArrayUnique) === -1) wordArrayUnique.push(el);
+	})
+	return wordArrayUnique.length;
+}
+
+// Calculate average word length
+function averageLength() {
+	var withoutSpace = textToEvaluate.replace(/ /g,"");
+	return (withoutSpace.length)/wordCountAll;
+}
+
+$(doEvaluation);
